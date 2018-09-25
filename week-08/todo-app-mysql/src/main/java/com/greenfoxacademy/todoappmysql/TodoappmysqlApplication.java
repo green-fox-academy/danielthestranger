3384a0e28ca3644ef1d32ee15c3bfc39b4c87237
danielthestranger <danielthestranger@gmail.com>
2018-09-25 16:08:54 +0200

    chore(todoappmysql): initial commit; MySQL connection properties not defined yet

---
+++ week-08/todo-app-mysql/src/main/java/com/greenfoxacademy/todoappmysql/TodoappmysqlApplication.java
@@ -0,0 +1,12 @@
+package com.greenfoxacademy.todoappmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappmysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoappmysqlApplication.class, args);
+    }
+}
---
+++ week-08/todo-app-mysql/src/main/java/com/greenfoxacademy/todoappmysql/models/Todo.java
@@ -0,0 +1,61 @@
+package com.greenfoxacademy.listingtodoswithh2.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+import org.springframework.cglib.core.GeneratorStrategy;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Todo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+    String title;
+    boolean urgent;
+    boolean done;
+
+    public Todo() {
+    }
+
+    public Todo(String title) {
+        this();
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isUrgent() {
+        return urgent;
+    }
+
+    public void setUrgent(boolean urgent) {
+        this.urgent = urgent;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
