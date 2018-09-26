82e64616bd1c10bd94f356f56fb5d8a09e672fae
danielthestranger <danielthestranger@gmail.com>
2018-09-26 13:23:22 +0200

    chore(application.properties): externalise config to separate .properties files

---
+++ week-08/todo-app-mysql/SetEnvVariables.bat
@@ -0,0 +1,8 @@
+SetX APP_DB_PATH jdbc:mysql://localhost/todo
+SetX APP_DB_USER dbadmin
+SetX APP_DB_USER_PASSWORD dbadmin
+SetX APP_HIBERNATE_DDL_AUTO create-drop
+SetX APP_HIBERNATE_DIALECT org.hibernate.dialect.MySQL5Dialect
+SetX APP_HIBERNATE_LOGGING_LEVEL debug
+SetX APP_HIBERNATE_SHOW_SQL true
+SetX APP_ENVIRONMENT localdev
